f1309256b0ab984ed8aa33be3026abc92617d0ae
sumanwave <suman.waveinfotech@gmail.com>
2025-06-19 11:23:23 +0530

    home layout setup

---
+++ src/components/layouts/homelayout/HomeLayout.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const HomeLayout = () => {
+  return (
+    <>
+    <div>
+        header
+    </div>
+
+    <div>
+        Footer
+    </div>
+    </>
+  )
+}
+
+export default HomeLayout
