b16b2f60a5d97f27a0acd3896449484dcb2f38cb
sumanwave <suman.waveinfotech@gmail.com>
2025-06-20 09:41:01 +0530

    worked on header

--- src/components/layouts/homelayout/HomeLayout.js
+++ src/components/layouts/homelayout/HomeLayout.js
@@ -1,10 +1,11 @@
 import Footer from "@/components/footer/Footer";
+import Header from "@/components/header/Header";
 import React from "react";
 
 const HomeLayout = () => {
   return (
     <>
-      <div>header</div>
+      <Header/>
 
       {/* Footer */}
       <Footer />
